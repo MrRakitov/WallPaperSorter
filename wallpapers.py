27d6930665370e2e90361ff7c9b2a5ca13c8a771
Stanislav Rakitov <rakitov.s@gmail.com>
2015-10-12 16:49:55 +0300

    Filetype check added

--- wallpapers.py
+++ wallpapers.py
@@ -8,22 +8,15 @@ import imghdr
 #Get all file names in current directory
 # Получить имена всех файлов в папке. Внести их в массив.
 #Use sample folder
-filenames = glob.glob('samplepic\*.jpg') #or use just current folder filenames = glob.glob('*.jpg')
+filenames = glob.glob('samplepic\*.*') #or use just current folder filenames = glob.glob('*.jpg')
 
-#Print each file name separately.
+#Print each file name separately. 
+#check is file an image or not.
 for fname in filenames:
   image_type = imghdr.what(fname)
   if not image_type:
-    print ("error")
+    print (fname + " is NOT an image file")
   else:
-    print (image_type)
-  
-  print (fname)
+    print (fname + " IS a " + image_type + " file")
+    # print (fname)
 
-
-# Пройтись по массиву. 
-# 	Открыть каждый файл.
-# 	Прочесть заголовок. 
-# 	Размер файла внести во временную переменную.
-# 	Закрыть файл.
-# 	Вывести название файла + размер.
