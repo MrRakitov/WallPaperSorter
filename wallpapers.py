4b82fefa8729fc32f657f0180385c83b83a11782
Stanislav Rakitov <rakitov.s@gmail.com>
2015-10-12 14:59:40 +0300

    Add some comments

--- wallpapers.py
+++ wallpapers.py
@@ -1,3 +1,11 @@
+#WallPaperSorter.py
+#@author Rakitov Stanislav
+#@URL https://github.com/MrRakitov/WallPaperSorter
+
 import glob
+
+#Get all file names in current directory
 filenames = glob.glob('*.jpg')
+
+#print all file names of current directory
 print (filenames)
